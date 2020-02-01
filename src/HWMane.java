60e1e1d821a2625590cd46844ef4fa446883b57c
Artem Sharov <sharov.a.p.10@gmail.com>
2020-02-01 21:58:18 +0300

    Workpiece

---
+++ src/HWMane.java
@@ -0,0 +1,12 @@
+public class HWMane {
+    public static void main(String[] args) {
+        method_1();
+        method_2();
+    }
+    public static void method_1(){
+
+    }
+    public static void method_2(){
+
+    }
+}
